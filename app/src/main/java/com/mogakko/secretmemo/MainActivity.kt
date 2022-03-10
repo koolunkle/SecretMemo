602d88d47ab69b76d1bbb5a2c3570ae4746b2fa2
김준기 <gugi329@daum.net>
2022-03-10 15:47:06 +0900

    이메일 및 비밀번호 체크

--- app/src/main/java/com/mogakko/secretmemo/MainActivity.kt
+++ app/src/main/java/com/mogakko/secretmemo/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.mogakko.secretmemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.mogakko.secretmemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        binding.btnConfirm.setOnClickListener {
+            val email = binding.edtInputEmail.text.toString()
+            val password = binding.edtInputPassword.text.toString()
+            if (email == "koolunkle@test.com") {
+                if (password == "koolunkle") {
+                    Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(mContext, "로그인 실패: 비밀번호 오류", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(mContext, "로그인 실패: 이메일 오류", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun setValues() {
+
     }
 }
