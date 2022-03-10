40b3227c770712576de21b8513584ec130b1da49
김준기 <gugi329@daum.net>
2022-03-10 15:52:34 +0900

    메모 작성 및 공유

--- app/src/main/java/com/mogakko/secretmemo/MemoActivity.kt
+++ app/src/main/java/com/mogakko/secretmemo/MemoActivity.kt
@@ -1,11 +1,35 @@
 package com.mogakko.secretmemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mogakko.secretmemo.databinding.ActivityMemoBinding
 
-class MemoActivity : AppCompatActivity() {
+class MemoActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMemoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_memo)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        binding.btnShare.setOnClickListener {
+            val memo = binding.edtInputMemo.text.toString()
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, memo)
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+    }
+
+    override fun setValues() {
+
     }
 }
